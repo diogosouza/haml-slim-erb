ccb6608406d7434ebc3f02ab92536ac86a84d994
Diogo Souza <diogosouzac@gmail.com>
2020-06-19 11:30:43 -0300

    apllying suggested changes

---
+++ app/controllers/constants.rb
@@ -0,0 +1,485 @@
+class Constants
+    NOTES = '[
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },
+        {
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        },{
+            "title": "Walk the dog",
+            "description": "Bla bla",
+            "tasks": [{
+                "title": "Task #1"
+            },
+            {
+                "title": "Task #2"
+            },
+            {
+                "title": "Task #3"
+            }]
+        }
+    ]
+    '
+end
